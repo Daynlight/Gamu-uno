be98f81aa4418384e6b18b31379deb1a8edb5c1f
DAYNLIGHT (✿◡‿◡) <danielstodulski000@gmail.com>
2025-05-26 00:59:09 +0200

    Better movement
    camera velocity offset, base stamina system, organise code

--- Game Uno/Assets/Player/Player.cs
+++ Game Uno/Assets/Player/Player.cs
@@ -1,45 +1,51 @@
 using UnityEngine;
 
+// [NOTE] Maybe O should separate Player and Movement
+
 public class Movement : MonoBehaviour
 {
-    ////////////////////////////// [Player] //////////////////////////////
+    ////////////////////////////// [Player Attributes] //////////////////////////////
     [SerializeField] private float move_speed = 6.0f;
     [SerializeField] private float move_acceleration = 15.0f;
     [SerializeField] private float sprint_speed = 10.0f;
     [SerializeField] private float sprint_acceleration = 20.0f;
     [SerializeField] private float dash_power = 20.0f;
     [SerializeField] private float dash_cooldown = 5;
-    [SerializeField] private float camera_offset_cof = 5;
+    [SerializeField] private float max_stamina = 30;
+    [SerializeField] private float stamina_recovery = 1;
+
+    ////////////////////////////// [Class Attributes] //////////////////////////////
+    [SerializeField] private float sprint_stamina_consumption = 5;
+    [SerializeField] private float stamina_penalty = 2;
+    [SerializeField] private float dash_stamina_consumption = 10;
+    [SerializeField] private float camera_offset_speed = 5;
 
     // [References]
-    [SerializeField] private Rigidbody rigidbody;
-    [SerializeField] private Camera camera;
+    private Rigidbody rb;
+    private Camera cam;
 
     //  [Local]
+    private float stamina = 0;
     private float dash_cooldown_time = 0;
     private Vector3 camera_offset = new Vector3(0, 0, 0);
+    private Vector3 last_position = new Vector3(0, 0, 0);
 
     ////////////////////////////// [Main] //////////////////////////////
     void Start()
     {
-        camera_offset = camera.transform.position - this.transform.position;
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+
+        camera_offset = cam.transform.position - this.transform.position;
+        last_position = this.transform.position;
+
+        stamina = max_stamina;
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            applyForce(sprint_acceleration, sprint_speed);
-            applyResistance(sprint_speed);
-        }
-        else
-        {
-            applyForce(move_acceleration, move_speed);
-            applyResistance(move_speed);
-        }
-
-        dash();
-        cameraOffset();
+        staminaControl();
+        movement();
     }
 
     ////////////////////////////// [Base Functions] //////////////////////////////
@@ -50,43 +56,72 @@ public class Movement : MonoBehaviour
         return direction_move;
     }
 
-    Vector3 getVelocity() {
-        return new Vector3(rigidbody.linearVelocity.x, 0, rigidbody.linearVelocity.z);   
+    Vector3 getVelocity()
+    {
+        return new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+    }
+
+    Vector3 getCursorDirectionFromObject()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, this.transform.position);
+        if (ground.Raycast(ray, out float distance))
+            return ray.GetPoint(distance);
+
+        Debug.LogError("Can't hit ground with ray");
+        return this.transform.position;
+    }
+
+    ////////////////////////////// [Movement] //////////////////////////////
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && stamina >= sprint_stamina_consumption)
+        {
+            moveForce(sprint_acceleration, sprint_speed);
+            stamina -= sprint_stamina_consumption * Time.deltaTime;
+            if (stamina < sprint_stamina_consumption) stamina -= stamina_penalty;
+        }
+        else moveForce(move_acceleration, move_speed);
+
+        dash();
+        cameraOffset();
+    }
+
+    // [Note] it takes too long to stop
+    void moveForce(float acceleration, float max_speed)
+    {
+        if (getVelocity().magnitude < Mathf.Sqrt(max_speed * max_speed)) rb.AddForce(getDirectionMove() * acceleration, ForceMode.Force);
+        float resistance_cof = Mathf.Clamp01(getVelocity().magnitude / max_speed);
+        if (max_speed != 0) rb.AddForce(-getVelocity() * resistance_cof, ForceMode.Force);
     }
 
     void cameraOffset()
     {
-        camera.transform.position = this.transform.position + camera_offset - getVelocity()/camera_offset_cof;
+        Vector3 camera_direction = this.transform.position - last_position;
+        Vector3 camera_velocity = camera_direction * camera_offset_speed * Time.deltaTime;
+        cam.transform.position = camera_offset + last_position;
+        last_position = last_position + camera_velocity;
     }
 
-    ////////////////////////////// [Movement] //////////////////////////////
-    void applyForce(float acceleration, float max_speed)
+    void staminaControl()
     {
-        if (getVelocity().magnitude < Mathf.Sqrt(max_speed * max_speed))
-            rigidbody.AddForce(getDirectionMove() * acceleration, ForceMode.Force);
-    }
-
-    void applyResistance(float max_speed)
-    {
-        float resistance_cof = Mathf.Clamp01(getVelocity().magnitude / max_speed);
-        if (max_speed != 0) rigidbody.AddForce(-getVelocity() * resistance_cof, ForceMode.Force);
+        if (stamina < max_stamina) stamina += stamina_recovery * Time.deltaTime;
+        if (stamina > max_stamina) stamina = max_stamina;
+        if (stamina < 0) stamina = 0;
+        print(stamina);
     }
 
     void dash()
     {
-       if(dash_cooldown_time > 0) dash_cooldown_time -= Time.deltaTime;
+        if (dash_cooldown_time > 0) dash_cooldown_time -= Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && dash_cooldown_time <= 0)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && stamina >= dash_stamina_consumption + stamina_penalty && dash_cooldown_time <= 0)
         {
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            Plane ground = new Plane(Vector3.up, transform.position);
-            if (ground.Raycast(ray, out float distance))
-            {
-                Vector3 dash_direction = ray.GetPoint(distance) - transform.position;
-                dash_direction /= dash_direction.magnitude;
-                rigidbody.AddForce(dash_direction * dash_power, ForceMode.Impulse);
-                dash_cooldown_time = dash_cooldown;
-            }
+            stamina -= dash_stamina_consumption;
+            if (stamina < sprint_stamina_consumption) stamina -= stamina_penalty;
+            Vector3 dash_direction = (getCursorDirectionFromObject() - this.transform.position).normalized;
+            rb.AddForce(dash_direction * dash_power, ForceMode.Impulse);
+            dash_cooldown_time = dash_cooldown;
         }
     }
 }
